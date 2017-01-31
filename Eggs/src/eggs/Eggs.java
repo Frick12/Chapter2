d192f655766941014ac3230454314c9b5c307049
Frick12 <harpodouglas@gmail.com>
2017-01-31 14:39:08 -0600

    Mad Libs and Eggs

---
+++ Eggs/src/eggs/Eggs.java
@@ -0,0 +1,30 @@
+package eggs;
+
+import javax.swing.JOptionPane;
+public class Eggs 
+{
+
+	public static void main(String[] args) 
+	{
+		String orderedString;
+		double dozenEggCost = 3.25;
+		double individualEggCost = .45;
+		int dozen = 12;
+		int numEggsOrdered;
+		int numDozenOrdered;
+		int numEggsRemainder;
+		double totalCost;
+		
+		orderedString = JOptionPane.showInputDialog(null, "How many eggs would you like?", JOptionPane.QUESTION_MESSAGE);
+		numEggsOrdered = Integer.parseInt(orderedString);
+		
+		numDozenOrdered = numEggsOrdered/dozen;
+		numEggsRemainder = numEggsOrdered % dozen;
+		totalCost = (dozenEggCost*numDozenOrdered) + (individualEggCost*numEggsRemainder);
+		
+		JOptionPane.showMessageDialog(null, "You ordered " + numEggsOrdered + " eggs. That's " + numDozenOrdered + " dozen at $" + dozenEggCost 
+				+ "\neach and " + numEggsRemainder + " loose eggs at $" + individualEggCost + " each for a total of $" + totalCost, orderedString, JOptionPane.INFORMATION_MESSAGE, null);
+
+	}
+
+}
---
+++ madLibs/src/madLibs/MadLibs.java
@@ -0,0 +1,33 @@
+package madLibs;
+
+import javax.swing.JOptionPane;
+public class MadLibs {
+
+	public static void main(String[] args) 
+	{
+		String color, wordEst, bodyPartPlural, animal, noun, pluralNoun, wholeNumber;
+		int a = 10, b, c;
+		
+		
+		color = JOptionPane.showInputDialog(null, "What is your favorite color?");
+		
+		wordEst = JOptionPane.showInputDialog(null, "Pick a word that ends in est.");
+		
+		bodyPartPlural = JOptionPane.showInputDialog(null, "Name a plural body part.");
+		
+		animal = JOptionPane.showInputDialog(null, "Name your favorite animal.");
+		
+		noun = JOptionPane.showInputDialog(null, "Pick a noun.");
+		
+		pluralNoun = JOptionPane.showInputDialog(null, "Pick a plural noun.");
+		
+		wholeNumber = JOptionPane.showInputDialog(null, "Give me a number that is a \nwhole number less than 10, no decimals.");
+		
+		b = Integer.parseInt(wholeNumber);
+		c = a - b;
+		
+		JOptionPane.showMessageDialog(null, "The " + color + " Dragon is the " + wordEst + " Dragon of all. It has " + c + " " + bodyPartPlural + ", and a "
+				+ animal + "\nshaped like a " + noun + ". It loves to eat " + pluralNoun + ", \nalthough it will feast on nearly anything.");
+	}
+
+}
